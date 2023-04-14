b1f4b09cfbef79aa98f5e211622c846e1bfef723
Jinwen Wang <jinwen.wang@wustl.edu>
2023-04-14 13:39:46 -0500

    backup

---
+++ rodinia_3.1/cuda/dwt2d/run.sh
@@ -0,0 +1,6 @@
+./dwt2d ../../data/dwt2d/192.bmp -d 192x192 -f -5 -l 3
+ls
+./dwt2d ../../data/dwt2d/rgb.bmp -d 1024x1024 -f -5 -l 3
+
+
+./dwt2d -d 192x192 -f 5 -l 3 ../../data/dwt2d/192.bmp ./output.txt
